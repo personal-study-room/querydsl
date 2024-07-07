7e66ebc4744ebff6b96b9b2bef8b9aa3a4280298
hongsh429 <shh0429@kakao.com>
2024-07-08 08:36:42 +0900

    기본 Q-type 활용 - 최초에 하나를 만들어 놓는다. generate 폴더 확인
    검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -13,7 +14,6 @@ import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 @SpringBootTest
@@ -68,12 +68,38 @@ public class QuerydslBasicTest {
 
   @Test
   void startQuerydsl() {
-    QMember m = new QMember("m");
-
     Member findMember = queryFactory
-        .select(m)
-        .from(m)
-        .where(m.username.eq("member1"))
+        .select(member)
+        .from(member)
+        .where(member.username.eq("member1"))
+        .fetchOne();
+
+    assertThat(findMember.getUsername()).isEqualTo("member1");
+  }
+
+  @Test
+  void search() {
+    Member findMember = queryFactory
+        .select(member)
+        .from(member)
+        .where(
+            member.username.eq("member1")
+                .and(member.age.eq(10))
+        )
+        .fetchOne();
+
+    assertThat(findMember.getUsername()).isEqualTo("member1");
+  }
+
+  @Test
+  void searchAndParam() {
+    Member findMember = queryFactory
+        .select(member)
+        .from(member)
+        .where(
+            member.username.eq("member1"),
+            member.age.eq(10)
+        )
         .fetchOne();
 
     assertThat(findMember.getUsername()).isEqualTo("member1");
